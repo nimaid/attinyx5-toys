68c0caab50d4e4771e20c7551a8fcdecc7a7b069
Ella Jameson <ellagjameson@gmail.com>
2024-06-01 09:33:49 -0700

    SO CLOSE to good power management

--- ATtiny85 Sound Toy Creator/examples/attiny85_bluey_yes_no_button/attiny85_bluey_yes_no_button.ino
+++ ATtiny85 Sound Toy Creator/examples/attiny85_bluey_yes_no_button/attiny85_bluey_yes_no_button.ino
@@ -12,7 +12,9 @@
  * 
  * 
  * 
- * Clock Setting: Internal 8 MHz
+ * Fuse Settings:
+ * • Internal 8 MHz
+ * • Disable millis() and micros() to save space
  * 
  * Schematic:
  *      ┌───U───┐
@@ -63,12 +65,6 @@
 // │ 1 │   No   │
 // └───┴────────┘
 
-// How many audio samples to use
-#define NUM_SAMPLES 2
-
-// Which pins will trigger each sample
-uint8_t trigger_pins[] = {3, 4};
-
 // Tweak this between 0 and 1023 to get the correct pitch for your samples
 // Higher is faster/higher pitched
 #define PITCH 850
@@ -78,11 +74,17 @@ uint8_t trigger_pins[] = {3, 4};
 
 
 
+#define DEBUG_LED 2
+
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 
 #include "samples.h"  // The sample audio data
+#define NUM_SAMPLES 2
+
+// Which pins will trigger each sample, tied to hard-coded ISRs
+uint8_t trigger_pins[] = {3, 4};
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -119,6 +121,8 @@ bool pin_state[NUM_SAMPLES] = {HIGH};
 uint8_t isr_run = 0;
 uint8_t i;
 
+
+
 void setup() {
   OSCCAL=255;
   // Enable 64 MHz PLL and use as the source for Timer1
@@ -132,51 +136,84 @@ void setup() {
 
   pinMode(1, OUTPUT);  // Enable PWM output pin
 
-  for(i=0; i<NUM_SAMPLES; i++) {
-    pinMode(trigger_pins[i], INPUT_PULLUP);
-  }
-
-  //Set up Timer/Counter0 for 20kHz interrupt to output samples.
+  // Set up Timer/Counter0 for 20kHz interrupt to output samples.
   TCCR0A = 3<<WGM00;             // Fast PWM
   TCCR0B = 1<<WGM02 | 2<<CS00;   // 1/8 prescale
   TIMSK = 1<<OCIE0A;             // Enable compare match, disable overflow
   OCR0A = 49;                    // Divide by 400
 
   set_playback_speed(PITCH);
+
+  // Setup pins as inputs
+  for(i=0; i<NUM_SAMPLES; i++) {
+    pinMode(trigger_pins[i], INPUT_PULLUP);
+  }
+
+  disable_pin_interrupts();
+
+  pinMode(DEBUG_LED, OUTPUT);
+  digitalWrite(DEBUG_LED, HIGH);
 }
 
-void loop() {
-  if (RingCount<32) {  // If there is space in ringbuffer
-    update_ring_buffer();
+//uint64_t count_since_last_press = 0;
 
+void loop() {
+  if(RingCount < 32) {  // If there is space in ringbuffer
+    update_ring_buffer();
+    
     // Detect the trigger inputs
     for(i=0; i<NUM_SAMPLES; i++) {
-      // Detect a_trigger state change
+      // Detect this pin state change
       if (digitalRead(trigger_pins[i]) != pin_state[i]) {  
         pin_state[i] = !pin_state[i]; // Toggle state
         // If on a falling edge, trigger the sample
         if (!pin_state[i]) {
+          //count_since_last_press = 0;
           play_sample(i);
         }
       }
     }
   }
+
+  // I just have to detect when the samples are no longer playing somehow and sleep then
+  // Maybe I should just add a simple timeout from the last button press. (Admit Defeat)
+  // Actually, it clicks anway when sleeping, so yeah, a delay is good
+  // I still want the delay to begin AFTER the audio buffer empties
+  // Oh god, I don't have time funcs because the timer is in use
+  /*
+  if(count_since_last_press > (1 << 100000000000000000000000000000000000000000000000000000000)) {
+    count_since_last_press = 0;
+    sleep();
+  } else {
+    count_since_last_press++;
+  }
+  */
 }
+
+
 
 ISR(TIMER0_COMPA_vect) {
   //-------------------  Ringbuffer handler -------------------------
     if(isr_run == ISR_SKIP) {
-      if (RingCount) {                            // If entry in FIFO
+      isr_run = 0;
+
+      if(RingCount) {                            // If entry in FIFO
         OCR1A = Ringbuffer[(RingRead++)];         // Output 8-bit DAC
         RingCount--;
       }
-
-      isr_run = 0;
     }
     else {
       isr_run++;
     }
   //-----------------------------------------------------------------
+}
+
+// Wakeup handlers
+ISR(PCINT3_vector) {
+  disable_sleep();
+}
+ISR(PCINT4_vector) {
+  disable_sleep();
 }
 
 void append_ring_buffer(uint8_t sample) {
@@ -209,4 +246,34 @@ void play_sample(uint8_t sample_num) {
 // 0-1023, 1023 is full
 void set_playback_speed(uint16_t playback_speed) {
   OCR0A = 49+((127- (playback_speed>>3) ));
+}
+
+void enable_pin_iterrupts() {
+  GIMSK |= (1 << PCIE);  // Enable extrnal interrupt on all enabled pins
+  PCMSK |= (1 << PCINT3) | (1 << PCINT4);  // Enable wakeup interrupt
+  //MCUCR |= (1 << ISC01);  // Change mode to falling edge detection
+}
+
+void disable_pin_interrupts() {
+  GIMSK &= ~(1 << PCIE); // Disable extrnal interrupt on all enabled pins
+  PCMSK &= ~(1 << PCINT3) & ~(1 << PCINT4);  // Disable wakeup interrupt
+}
+
+void sleep()
+{ 
+  digitalWrite(DEBUG_LED, LOW);
+  enable_pin_iterrupts();
+
+  MCUCR |= (1 << SM1);      // enabling sleep mode and powerdown sleep mode
+  MCUCR |= (1 << SE);     //Enabling sleep enable bit
+  __asm__ __volatile__ ( "sleep" "\n\t" :: ); //Sleep instruction to put controller to sleep
+  //controller stops executing instruction after entering sleep mode  
+}
+
+void disable_sleep() {
+  digitalWrite(DEBUG_LED, HIGH);
+  disable_pin_interrupts();
+
+  MCUCR &= ~(1 << SM1);      // enabling sleep mode and powerdown sleep mode
+  MCUCR &= ~(1 << SE);
 }
